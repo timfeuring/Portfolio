192b5e21e3ebb4265b16ae883c09402161281203
Tim Feuring <timfeu@timfeu.localdomain>
2020-02-04 20:37:55 +0100

    updated Swipe Studio + Sauerwein

---
+++ _site/assets/script.js
@@ -0,0 +1,27 @@
+// SET ACTIVE PAGE
+// ==========================================================================================
+const navAbout = document.getElementById('nav-about');
+const navWork = document.getElementById('nav-work');
+
+function setActivePage(a, b) {
+    a.classList.add('active-page');
+    b.classList.remove('active-page');
+}
+
+
+
+function closeBurgerNav() {
+    document.getElementById('main-nav__trigger').checked = false;
+}
+
+var i = 0;
+var txt =  ", I'm Tim. "
+var speed = 100;
+
+setTimeout(function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("about-headline").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}, 1000)
